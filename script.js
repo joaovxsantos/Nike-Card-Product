7a595b33e67e472ce044ac536d18673527080c94
João Vitor <joaovitor55670@gmail.com>
2024-05-14 19:37:10 -0300

    Ajutes e funcionalidade para trocar o valor dependendo do size

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colorButtons = document.querySelectorAll('.color');
 const images = document.querySelectorAll('.images img');
 const body = document.body;
 const sizeButtons = document.querySelectorAll('.button-nums span');
-
+const priceElement = document.querySelector('#price');
 
 colorButtons.forEach((button, indx) => {
     button.addEventListener('click', () => {
@@ -14,7 +14,19 @@ colorButtons.forEach((button, indx) => {
 
 sizeButtons.forEach((sizeButton) => {
     sizeButton.addEventListener('click', () => {
+
+        const valueSize = +sizeButton.innerHTML
+
         sizeButtons.forEach((img) => img.classList.remove('active'))
         sizeButton.classList.add('active');
+
+        if (valueSize <= 7) {
+            priceElement.innerHTML = '300,00'
+        }
+        else if (valueSize > 7 && valueSize < 10) {
+            priceElement.innerHTML = '350,00'
+        } else {
+            priceElement.innerHTML = '450,00'
+        }
     })
 })
