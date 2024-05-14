9a2bfc7a8000ad6d99b6f5f3e28bc7a261c3bdf6
João Vitor <joaovitor55670@gmail.com>
2024-05-14 18:03:17 -0300

    Funcionalidade para que troque de tenis e estilo quando clicado em algum dos botoes

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const colorButtons = document.querySelectorAll('.color');
 const images = document.querySelectorAll('.images img');
 const body = document.body;
 const sizeButtons = document.querySelectorAll('.button-nums span');
+
+
+colorButtons.forEach((button, indx) => {
+    button.addEventListener('click', () => {
+        images.forEach((img) => img.classList.remove('active'))
+        indx === 0 ? body.classList = '' : body.classList = 'change'
+        images[indx].classList.add('active')
+    })
+})
