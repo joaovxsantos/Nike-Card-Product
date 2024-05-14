f612250e6db2940ab8e59fa5660061de3c4d16ba
João Vitor <joaovitor55670@gmail.com>
2024-05-14 18:02:46 -0300

    Utilizando query selector para pegar os elementos necessarios

---
+++ script.js
@@ -0,0 +1,4 @@
+const colorButtons = document.querySelectorAll('.color');
+const images = document.querySelectorAll('.images img');
+const body = document.body;
+const sizeButtons = document.querySelectorAll('.button-nums span');
